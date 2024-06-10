bb25dc4080b47050ea13f8562d7cde2f72f43e69
friedliver <arnobmallick@gmail.com>
2024-06-10 16:43:48 -0700

    Update run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,11 +1,11 @@
 # databricks secrets
-export DATABRICKS_HOST="https://dbc-55d4f7d9-4ff3.cloud.databricks.com"
-export DATABRICKS_TOKEN="dapi3dd415a31c37144bc9be143426f4bf7c"
+export DATABRICKS_HOST="https://<fill-me>.databricks.com"
+export DATABRICKS_TOKEN=""
 
 # neo4j secrets
 export NEO4J_PROTOCOL="neo4j+s://"
-export NEO4J_CONNECTION_URL="f8e4edff.databases.neo4j.io:7687"
-export NEO4J_USER="neo4j"
-export NEO4J_PASSWORD="6ooZpGABKyBC5WAFDSnpcm83r39ZLQuMWLpHNRWGDuE"
+export NEO4J_CONNECTION_URL=""
+export NEO4J_USER=""
+export NEO4J_PASSWORD=""
 
 streamlit run app.py
